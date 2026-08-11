cc3c9ea610a4d100f5255d87f018baf5e6247af4
aaditya1606 <aadityashakya.tech@yahoo.com>
2026-08-11 22:11:15 +0530

    ELEMENT WISE SUM OF TWO ARRAYS

---
+++ sum_of_arrays.c
@@ -0,0 +1,54 @@
+/*
+PROGRAM: FIND THE ELEMENT-WISE SUM OF TWO ARRAYS(A[i]+B[i])
+LANGUAGE: C
+AUTHOR- AADITYA SHAKYA
+*/
+
+#include <stdio.h>
+int main()
+{
+    int size1;
+    printf("ENTER THE SIZE OF THE FIRST ARRAY: ");
+    scanf("%d", &size1);
+    while (size1 <= 0)
+    {
+        printf("ENTER A VALID SIZE!! RE-ENTER: ");
+        scanf("%d", &size1);
+    }
+    int arr1[size1];
+    for (int i = 0; i < size1; i++)
+    {
+        printf("INSERT THE VALUE: ");
+        scanf("%d", &arr1[i]);
+    }
+    int size2;
+    printf("ENTER THE SIZE OF THE SECOND ARRAY: ");
+    scanf("%d", &size2);
+    while (size2 <= 0)
+    {
+        printf("ENTER A VALID SIZE!! RE-ENTER: ");
+        scanf("%d", &size2);
+    }
+    int arr2[size2];
+    for (int i = 0; i < size2; i++)
+    {
+        printf("INSERT THE VALUE: ");
+        scanf("%d", &arr2[i]);
+    }
+    int size;
+    if (size1 <= size2)
+    {
+        size = size1;
+    }
+    else
+    {
+        size = size2;
+    }
+    int sum[size];
+    for (int i = 0; i < size; i++)
+    {
+        sum[i] = arr1[i] + arr2[i];
+        printf("%d ", sum[i]);
+    }
+    return 0;
+}
